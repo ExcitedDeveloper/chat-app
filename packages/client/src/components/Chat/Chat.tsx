9dbcda260ed58e3d97b7bd2f0d0b40ef31ee21eb
Steve Hunley <shunley2000@yahoo.com>
2023-01-18 07:08:50 -0600

    Fix submit button

--- packages/client/src/components/Chat/Chat.tsx
+++ packages/client/src/components/Chat/Chat.tsx
@@ -1,6 +1,7 @@
 import './Chat.css'
 
 const Chat = () => {
+  console.log(`render Chat`)
   return <div>Chat</div>
 }
 
--- packages/client/src/components/JoinRoom/JoinRoom.tsx
+++ packages/client/src/components/JoinRoom/JoinRoom.tsx
@@ -1,3 +1,5 @@
+import { FormEvent } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSmile } from '@fortawesome/free-solid-svg-icons'
 import Button from 'react-bootstrap/Button'
@@ -5,6 +7,15 @@ import Form from 'react-bootstrap/Form'
 import './JoinRoom.css'
 
 const JoinRoom = () => {
+  const navigate = useNavigate()
+
+  const handleSubmit = (e: any) => {
+    console.log(`handleSubmit`)
+    e.preventDefault()
+    navigate('/chat')
+  }
+
+  console.log(`render JoinRoom`)
   return (
     <div className='join-container'>
       <header className='join-header'>
@@ -13,17 +24,15 @@ const JoinRoom = () => {
         </h1>
       </header>
       <main className='join-main'>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group controlId='username' className='mb-3'>
             <Form.Label>Username</Form.Label>
             <Form.Control placeholder='Enter username...' />
           </Form.Group>
           <Form.Group controlId='room' className='mb-5'>
             <Form.Label>Room</Form.Label>
-            <Form.Select>
-              <option value='JavaScript' selected>
-                JavaScript
-              </option>
+            <Form.Select defaultValue={'JavaScript'}>
+              <option value='JavaScript'>JavaScript</option>
               <option value='Python'>Python</option>
               <option value='PHP'>PHP</option>
               <option value='C#'>C#</option>
@@ -35,7 +44,7 @@ const JoinRoom = () => {
             as='input'
             type='submit'
             value='Join Chat'
-            className='w-full join-submit'
+            className='d-block w-100 join-submit'
           />
         </Form>
       </main>
