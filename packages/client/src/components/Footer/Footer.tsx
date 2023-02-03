8f067b302f706130c12de38ddd438205c252fbb4
Steve Hunley <shunley2000@yahoo.com>
2023-02-03 08:08:53 -0600

    Style footer

--- packages/client/src/components/Footer/Footer.tsx
+++ packages/client/src/components/Footer/Footer.tsx
@@ -1,7 +1,27 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane } from '@fortawesome/free-regular-svg-icons'
 import './Footer.css'
 
 const Footer = () => {
-  return <footer>Footer</footer>
+  return (
+    <footer>
+      <div className='chat-form-container'>
+        <form id='chat-form' className='chat-form'>
+          <input
+            id='msg'
+            type='text'
+            required
+            autoComplete='off'
+            placeholder='Enter Message'
+          />
+          <button className='btn'>
+            <FontAwesomeIcon icon={faPaperPlane} className='chat-paperplane' />
+            {' Send'}
+          </button>
+        </form>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
